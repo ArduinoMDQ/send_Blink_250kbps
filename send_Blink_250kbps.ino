2bdc546490cd06ea7661045b757d1fe964dc1cdf
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2016-08-06 11:33:16 -0300

    first cmm

---
+++ send_Blink_250kbps.ino
@@ -0,0 +1,66 @@
+// demo: CAN-BUS Shield, send data
+#include <mcp_can.h>
+#include <SPI.h>
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+const int LED=5;
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    Serial.begin(9600);
+     pinMode(LED,OUTPUT);
+     digitalWrite(LED,LOW);
+
+START_INIT:
+
+    if(CAN_OK == CAN.begin(CAN_250KBPS,MCP_8MHz))                   // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init ok!");
+          digitalWrite(LED,true);
+           delay(200);
+           digitalWrite(LED,false);
+           delay(200);
+           digitalWrite(LED,true);
+           delay(200);
+           digitalWrite(LED,false);
+    }
+    else
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println("Init CAN BUS Shield again");
+        digitalWrite(LED,true);
+           delay(100);
+           digitalWrite(LED,false);
+           delay(200);
+           digitalWrite(LED,true);
+           delay(100);
+           digitalWrite(LED,false);
+           delay(200);
+           digitalWrite(LED,true);
+           delay(800);
+            digitalWrite(LED,false);
+           delay(200);
+        goto START_INIT;
+    }
+}
+
+unsigned char stmpON[8] = {0,0,1,0,0,0,0,0};
+unsigned char stmpOFF[8] = {0,0,0,0,0,0,0,0};
+
+void loop()
+{   Serial.println("In loop");
+    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
+//    CAN.sendMsgBuf(0x80,0, 8, stmpON);
+      digitalWrite(LED,HIGH);
+    delay(4000);                       // send data per 100ms
+ //    CAN.sendMsgBuf(0x80,0, 8, stmpOFF);
+    delay(4000);                       // send data per 100ms
+     digitalWrite(LED,LOW);
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
